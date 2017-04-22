c2d07bb0cbf46bb9bf9d8eb3ac69d9bdcf44f0db
blueming <wenyiff66@gmail.com>
2017-04-22 17:36:57 +0800

    denglu tanchuang design

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,19 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def resource_name
+    :user
+  end
+
+  def resource
+      @resource ||= User.new
+  end
+
+  def resource_class
+      User
+  end
+
+  def devise_mapping
+      @devise_mapping ||= Devise.mappings[:user]
+  end
 end
